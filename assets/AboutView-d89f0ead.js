1eefc77200ead09fe54980ccc382177ea6290df9
Samind2 <111554627+Samind2@users.noreply.github.com>
2025-03-11 10:27:34 +0000

    Deploying to gh-pages from @ Samind2/gt2@7edb9bb45e5b1b057aae911c0634bcb9a9e2c08a 🚀

---
+++ assets/AboutView-d89f0ead.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as o,a as _}from"./index-f3abf10b.js";const s={},c={class:"about"},n=_("h1",null,"github action",-1),a=[n];function i(r,u){return t(),o("div",c,a)}const l=e(s,[["render",i]]);export{l as default};
